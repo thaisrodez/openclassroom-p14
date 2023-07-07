ce23febc594adcdb8caefa8afa16a961a8074d9d
thais de rodez <thaisderodez@gmail.com>
2023-07-07 11:14:27 +0100

    First configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+/* eslint-env node */
+
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "eslint-config-prettier",
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: {
+    react: { version: "18.2" },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx"],
+      },
+    },
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "space-before-function-paren": "off",
+    "comma-dangle": "off",
+  },
+};
