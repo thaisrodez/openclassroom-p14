ade260d6c1901843b3f162305ce678e546450a07
thais de rodez <thaisderodez@gmail.com>
2023-07-07 15:06:48 +0100

    feat(table): add table package for employee list

---
+++ src/pages/employeeList.jsx
@@ -0,0 +1,16 @@
+import { useMemo } from 'react'
+import { Header } from '../components/header'
+import { mockEmployees, tableHeaders } from '../data/mockData'
+import { useSelector } from 'react-redux'
+import { Table } from 'paginated-table-react-pkg'
+
+export function EmployeeList() {
+  const newEmployees = useSelector((state) => state.employees)
+  const getEmployees = useMemo(() => newEmployees.concat(mockEmployees), [newEmployees])
+  return (
+    <>
+      <Header title='Current Employees' linkLabel='Home' linkPath='/' />
+      <Table data={getEmployees} tableHeaders={tableHeaders} />
+    </>
+  )
+}
