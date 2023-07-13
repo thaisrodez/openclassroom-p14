62c4e323a260eab98b8492bff8f81d8c7fc12f60
thais de rodez <thaisderodez@gmail.com>
2023-07-13 14:03:37 +0200

    fix(tailwind): add tailwind config for package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import "./node_modules/paginated-table-react-pkg"
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/paginated-table-react-pkg/**/*.{html,js, jsx}"
+  ],
   theme: {
     extend: {},
   },
